ee09db6ce65d93bea8b9d64384fae0bcd328f05e
Romich <alligarch@gmail.com>
2020-04-18 18:27:08 +0300

    first

---
+++ build/precache-manifest.b5a306c8a948c8294f58985f3979250f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2de3fd659ab872c582a57026a7ed2c",
+    "url": "/zakypu/index.html"
+  },
+  {
+    "revision": "a2574d1a143a17e356c8",
+    "url": "/zakypu/static/css/main.cfe6b972.chunk.css"
+  },
+  {
+    "revision": "cc0b32ba4b8595bffd83",
+    "url": "/zakypu/static/js/2.bb16d8ff.chunk.js"
+  },
+  {
+    "revision": "a2574d1a143a17e356c8",
+    "url": "/zakypu/static/js/main.4657c339.chunk.js"
+  },
+  {
+    "revision": "84c7bb83903d3480ff04",
+    "url": "/zakypu/static/js/runtime~main.a6dcf39e.js"
+  }
+]);
